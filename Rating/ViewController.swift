8ce7bab26436f44f802127437811b1efc1c4625c
Tom Malary <tom@Toms-MacBook-Air.local>
2014-11-27 20:34:33 +0300

    done for yes button except alert

--- Rating/ViewController.swift
+++ Rating/ViewController.swift
@@ -10,20 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var votingStatus: Bool?
+    
     @IBOutlet var evkurov: UIImageView!
     
     @IBAction func yesButtonPressed(sender: AnyObject) {
-
-        // var score should be refactorred for both buttons
-        var score = PFObject(className: "score")
-        score.setObject(1, forKey: "vote")
-        score.saveInBackgroundWithBlock{
-            (succes: Bool!, error: NSError!) -> Void in
-            if succes == true {
-                println("Done with ID \(score.objectId)")
-            } else {
-                println(error)
+        
+        //Check if votingStatus is true
+        if votingStatus == true {
+        
+            var score = PFObject(className: "score")
+            score.setObject(1, forKey: "vote")
+            score.saveInBackgroundWithBlock{
+                (succes: Bool!, error: NSError!) -> Void in
+                if succes == true {
+                    println("Done with ID \(score.objectId)")
+                    //update NSUserdefaults
+                    NSUserDefaults.standardUserDefaults().setObject(NSDate(), forKey: "controlDate")
+                    NSUserDefaults.standardUserDefaults().synchronize()
+                    self.votingStatus = false
+                } else {
+                    println(error)
+                    //alert show
+                }
             }
+        } else {
+            println("Status is \(votingStatus)")
         }
     }
 
@@ -45,34 +57,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // Find if user haven't already voted within 1 day so if he/she are allowed to vote
-
-        var votingStatus: Bool?
+        // Find if user haven't already voted within 1 day so if his/her voting status true
+        // var votingStatus: Bool?
         
         //To retrive the control date value. First time it has nil value
         var controlDate = NSUserDefaults.standardUserDefaults().objectForKey("controlDate") as? NSDate
         var timeInterval = controlDate?.timeIntervalSinceNow
-        var dayInSeconds = 24.0 * 3600
+        var dayInSeconds = 24.0 * -3600
         
         //debug
         println(votingStatus)
 
         if controlDate == nil {
-        
             votingStatus = true
-            
         } else {
-            
-            if timeInterval < dayInSeconds {
-            
+            if timeInterval > dayInSeconds {
                 votingStatus = false
-                
             } else {
-            
                 votingStatus = true
-                
             }
-            
         }
         
         //debug
